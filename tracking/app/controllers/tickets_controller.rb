a465dcd7d66e31ebbc6b087b995356e698429409
alishkaLala <aly_6@ukr.net>
2013-05-06 10:15:11 +0300

    history styling

--- tracking/app/controllers/tickets_controller.rb
+++ tracking/app/controllers/tickets_controller.rb
@@ -25,11 +25,15 @@ class TicketsController < ApplicationController
  
   def show_ticket_by_reference
     @ticket = Ticket.find_by_reference(params[:reference])
-    @histories = @ticket.ticket_histories.order("created_at DESC").paginate(:per_page => 3, :page => params[:page])
-    @comment = Comment.new
-    @comments = @ticket.comments.order("created_at DESC").paginate(:page => params[:page_comment], :per_page => 3)
-    @statuses = TicketStatus.all 
-    render "/staff/tickets/show"  
+    if @ticket
+      @histories = @ticket.ticket_histories.order("created_at DESC").paginate(:per_page => 3, :page => params[:page])
+      @comment = Comment.new
+      @comments = @ticket.comments.order("created_at DESC").paginate(:page => params[:page_comment], :per_page => 3)
+      @statuses = TicketStatus.all 
+      render "/staff/tickets/show"  
+    else
+      redirect_to root_path
+    end
   end
   
   private
